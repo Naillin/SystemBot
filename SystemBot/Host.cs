04e9715f9ce5f5bfbcd1f1ed1a64b6c73f32add3
VladVlad9n <vlad.tyupin@yandex.ru>
2025-01-29 15:45:10 +0500

    keyboardcommit

---
+++ SystemBot/Host.cs
@@ -0,0 +1,45 @@
+﻿using IniParser.Model;
+using IniParser;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Polling;
+
+namespace SystemBot
+{
+    public class Host
+    {
+        public Action<ITelegramBotClient, Update>? OnMessage;
+
+        private static TelegramBotClient? _botClient;
+
+        public Host(string _token) 
+        {
+            _botClient = new TelegramBotClient(_token);
+        }
+
+        public void Start()
+        {
+            _botClient?.StartReceiving(UpdateHandler, ErrorHandler);
+            Console.WriteLine("Бот запущен");
+        }
+
+        private async Task ErrorHandler(ITelegramBotClient client, Exception exception, HandleErrorSource source, CancellationToken token)
+        {
+            Console.WriteLine("Ошибка: " + exception.Message);
+            await Task.CompletedTask;
+        }
+
+        private async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            Console.WriteLine($"Пришло сообщение: {update.Message?.Text ?? "[не текст]"}");
+            OnMessage?.Invoke(client, update);
+            await Task.CompletedTask;
+        }
+    }
+}
--- SystemBot/Program.cs
+++ SystemBot/Program.cs
@@ -2,64 +2,93 @@
 using IniParser;
 using NLog;
 using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using System;
 
 namespace SystemBot
 {
 	internal class Program
 	{
-		private static readonly string moduleName = "SystemBot.Program";
-		private static readonly Logger baseLogger = LogManager.GetLogger(moduleName);
-		private static readonly LoggerManager logger = new LoggerManager(baseLogger, moduleName);
 
-		private static string _token = string.Empty;
-		public static string TOKEN
-		{
-			get
-			{
-				return _token;
-			}
-		} 
-		public static int EXAMPLE1 { get; set; } = 6;
+        private static readonly string moduleName = "SystemBot.Program";
+        private static readonly Logger baseLogger = LogManager.GetLogger(moduleName);
+        private static readonly LoggerManager logger = new LoggerManager(baseLogger, moduleName);
 
-		private const string filePathConfig = "config.ini";
-		private static string configTextDefault = string.Empty;
-		private static void initConfig()
-		{
-			FileIniDataParser parser = new FileIniDataParser();
+        private static string _token = string.Empty;
+        public static string TOKEN
+        {
+            get
+            {
+                return _token;
+            }
+        }
+        public static int EXAMPLE1 { get; set; } = 6;
 
-			if (File.Exists(filePathConfig))
-			{
-				logger.Info($"Чтение конфигурационного файла.");
+        private const string filePathConfig = "config.ini";
+        private static string configTextDefault = string.Empty;
+        private static void initConfig()
+        {
+            FileIniDataParser parser = new FileIniDataParser();
 
-				IniData data = parser.ReadFile(filePathConfig);
-				_token = data["Settings"]["TOKEN"];
-				EXAMPLE1 = Convert.ToInt32(data["Settings"]["EXAMPLE1"]);
-			}
-			else
-			{
-				logger.Info($"Создание конфигурационного файла.");
+            if (File.Exists(filePathConfig))
+            {
+                logger.Info($"Чтение конфигурационного файла.");
 
-				IniData data = new IniData();
-				data.Sections.AddSection("Settings");
-				data["Settings"]["TOKEN"] = _token.ToString();
-				data["Settings"]["EXAMPLE1"] = EXAMPLE1.ToString();
-				parser.WriteFile(filePathConfig, data);
-			}
+                IniData data = parser.ReadFile(filePathConfig);
+                _token = data["Settings"]["TOKEN"];
+                EXAMPLE1 = Convert.ToInt32(data["Settings"]["EXAMPLE1"]);
+            }
+            else
+            {
+                logger.Info($"Создание конфигурационного файла.");
 
-			configTextDefault = $"TOKEN = [{_token}]\r\n" +
-								$"EXAMPLE1 = [{EXAMPLE1}]";
-		}
+                IniData data = new IniData();
+                data.Sections.AddSection("Settings");
+                data["Settings"]["TOKEN"] = _token.ToString();
+                data["Settings"]["EXAMPLE1"] = EXAMPLE1.ToString();
+                parser.WriteFile(filePathConfig, data);
+            }
 
-		private static ITelegramBotClient? _botClient;
-		static async Task Main(string[] args)
+            configTextDefault = $"TOKEN = [{_token}]\r\n" +
+                                $"EXAMPLE1 = [{EXAMPLE1}]";
+        }
+
+        
+        static async Task Main(string[] args)
 		{
 			logger.Info($"Starting...");
 			initConfig();
 			logger.Info(configTextDefault);
-
-			_botClient = new TelegramBotClient(_token);
-
+            
 			logger.Info($"Done!");
-		}
-	}
+
+            Host bot = new Host(_token);
+            bot.Start();
+            bot.OnMessage += OnMessage;
+
+            Console.ReadLine();
+
+            await Task.CompletedTask;
+        }
+
+        private static async void OnMessage(ITelegramBotClient client, Update update)
+        {
+            if (update.Message?.Text == "/start")
+            {
+                await client.SendMessage(update.Message.Chat.Id, "Здравствуйте, это бля бот!");
+                var replyKeyboard = new ReplyKeyboardMarkup(true).AddButton("Убрать клавиатуру");
+
+                await client.SendMessage(update.Message.Chat.Id, "Выберите действие", replyMarkup: replyKeyboard);
+
+            }
+            else if (update.Message?.Text == "Убрать клавиатуру")
+            {
+                await client.SendMessage(update.Message.Chat.Id, "Клавиатура убрана", replyMarkup: new ReplyKeyboardRemove()).ConfigureAwait(false);
+            }
+            
+        }
+    }
 }
