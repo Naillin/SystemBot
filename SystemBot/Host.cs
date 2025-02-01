7bce19931dfc8f3b6cbeb7b04d083736609d956c
Naillin <kamilasd@yandex.ru>
2025-02-01 19:08:34 +0500

    added logger in Host class

--- SystemBot/Host.cs
+++ SystemBot/Host.cs
@@ -14,7 +14,11 @@ namespace SystemBot
 {
     public class Host
     {
-        public Action<ITelegramBotClient, Update>? OnMessage;
+		private static readonly string moduleName = "Host";
+		private static readonly Logger baseLogger = LogManager.GetLogger(moduleName);
+		private static readonly LoggerManager logger = new LoggerManager(baseLogger, moduleName);
+
+		public Action<ITelegramBotClient, Update>? OnMessage;
 
         private static TelegramBotClient? _botClient;
 
@@ -26,19 +30,19 @@ namespace SystemBot
         public void Start()
         {
             _botClient?.StartReceiving(UpdateHandler, ErrorHandler);
-            Console.WriteLine("Бот запущен");
-        }
+            Console.WriteLine("Бот запущен"); //Используй логер вместо Console.WriteLine!!!!!!!!!!!!!
+		}
 
         private async Task ErrorHandler(ITelegramBotClient client, Exception exception, HandleErrorSource source, CancellationToken token)
         {
-            Console.WriteLine("Ошибка: " + exception.Message);
-            await Task.CompletedTask;
+            Console.WriteLine("Ошибка: " + exception.Message); //Используй логер вместо Console.WriteLine!!!!!!!!!!!!
+			await Task.CompletedTask;
         }
 
         private async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
         {
-            Console.WriteLine($"Пришло сообщение: {update.Message?.Text ?? "[не текст]"}");
-            OnMessage?.Invoke(client, update);
+            Console.WriteLine($"Пришло сообщение: {update.Message?.Text ?? "[не текст]"}"); //Используй логер вместо Console.WriteLine!!!!!!!!!
+			OnMessage?.Invoke(client, update);
             await Task.CompletedTask;
         }
     }
